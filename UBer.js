b7cf7fe7b70ee22a1eaccf8e04c8c247b7e17302
EnderKingJ#0001 <78042486+EnderKingJ@users.noreply.github.com>
2021-05-24 20:44:16 -0400

    Update UBer.js

--- UBer.js
+++ UBer.js
@@ -42,7 +42,6 @@ enter url.\
       };
     };
   }
-  }
   if ("" != url && null != url) {
     if (url.includes("//", 0) || url.includes("http://", 0) || url.includes("https://", 0)) {
       document.body.innerHTML = '<body style="margin:0px;padding:0px;overflow:hidden"><iframe src="' + url + '" id="uberFrame" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" height="100%" width="100%"></iframe></body>'

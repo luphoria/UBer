86ee86a1e7af4586f6e590a7501087a55d9190f2
MrBrain295 <66077254+MrBrain295@users.noreply.github.com>
2021-06-04 11:07:37 -0500

    tiny grammar fix

--- Lyft.js
+++ Lyft.js
@@ -1,5 +1,5 @@
 /* Lyft by luphoria */
 window.addEventListener('beforeunload', function (w) { 
   w.preventDefault(); // prompt "Leave site?"
-  return "shut up goguardian";
+  return "shut up GoGuardian";
 });

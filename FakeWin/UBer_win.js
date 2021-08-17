aeb638ec707ab0810d53b3906c2897cc1aecf9b5
luphoria <60309933+luphoria@users.noreply.github.com>
2021-08-17 01:12:50 -0500

    Update UBer_win.js

--- FakeWin/UBer_win.js
+++ FakeWin/UBer_win.js
@@ -4,11 +4,6 @@ let windowUrl = prompt("enter URL: \n   -luphoria")
 let domain = windowUrl.split("/")[2]
 let domain_params
 if (windowUrl.split("/")[3] != undefined) domain_params = "/" + windowUrl.split("/")[3]
-document.getElementById("url").onblur = function() {
-    windowUrl = document.getElementById("url").value
-    domain = windowUrl.split("/")[2]
-    if (windowUrl.split("/")[3] != undefined) domain_params = "/" + windowUrl.split("/")[3]
-}
 let win = new FakeWindow()
 win.bind(['a', 'button'])
 win.init()

4e75cea1b00d3dc4f37589a7c13f4b46313134aa
luphoria <60309933+luphoria@users.noreply.github.com>
2021-08-17 01:09:19 -0500

    Update UBer_win.js

--- FakeWin/UBer_win.js
+++ FakeWin/UBer_win.js
@@ -1,6 +1,6 @@
 import "./fake_window_proxy.js"
 (function() {
-let windowUrl = prompt("enter URL: \n   -luphoria", placeholder = "https://google.com/")
+let windowUrl = prompt("enter URL: \n   -luphoria")
 let domain = windowUrl.split("/")[2]
 let domain_params
 if (windowUrl.split("/")[3] != undefined) domain_params = "/" + windowUrl.split("/")[3]

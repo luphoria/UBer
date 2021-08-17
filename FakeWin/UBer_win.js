7a9113072061eb6f1b5469c9d878e1a207a150a7
luphoria <60309933+luphoria@users.noreply.github.com>
2021-08-17 00:19:43 -0500

    Create UBer_win.js

---
+++ FakeWin/UBer_win.js
@@ -0,0 +1,13 @@
+import "./fake_window_proxy"
+let windowUrl = prompt("enter URL: \n   -luphoria", placeholder = "https://google.com/")
+let domain = windowUrl.split("/")[2]
+let domain_params
+if (windowUrl.split("/")[3] != undefined) domain_params = "/" + windowUrl.split("/")[3]
+document.getElementById("url").onblur = function() {
+    windowUrl = document.getElementById("url").value
+    domain = windowUrl.split("/")[2]
+    if (windowUrl.split("/")[3] != undefined) domain_params = "/" + windowUrl.split("/")[3]
+}
+let win = new FakeWindow()
+win.bind(['a', 'button'])
+win.init()
